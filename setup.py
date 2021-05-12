a500836e70c61a2b978d61611c19603985239108
rursino <rosscursino@gmail.com>
2021-05-12 18:01:12 +1000

    Move analytics away from core pkgs

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='afl-stats',
       version='0.1',
       description='AFL stats completed by R.Ursino',
       author='Ross Ursino',
       author_email='rosscursino@gmail.com',
-      packages=['match_stats', 'player_stats'],
+      packages=find_packages(),
       package_data={
           'match_stats': ['data/matchstats.json']
         },
